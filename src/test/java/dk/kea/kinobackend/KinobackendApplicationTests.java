2f9de61d2bdb1d0a590d9bb387b1842b233be342
sebwelsch <sebjak1@hotmail.com>
2025-03-05 11:11:41 +0100

    inital spring boot commit

---
+++ src/test/java/dk/kea/kinobackend/KinobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.kinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
