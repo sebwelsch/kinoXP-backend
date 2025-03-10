f057d07c283de4240b1885baa1f46006fac004c4
sebwelsch <sebjak1@hotmail.com>
2025-03-10 12:45:13 +0100

    added in moviecontroller

--- src/main/java/dk/kea/kinobackend/controller/MovieController.java
+++ src/main/java/dk/kea/kinobackend/controller/MovieController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("movies")
 public class MovieController {
@@ -22,5 +24,10 @@ public class MovieController {
     public ResponseEntity<Movie> createMovie(@RequestBody Movie newMovie) {
         Movie savedMovie = movieRepository.save(newMovie);
         return new ResponseEntity<>(savedMovie, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
     }
 }
