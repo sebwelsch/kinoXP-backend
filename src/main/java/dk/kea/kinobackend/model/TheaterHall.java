ed774ddfc59ec2c9964cb64449a3230d1eff07d9
Sebastian <swj@Sebastians-MacBook-Pro.local>
2025-03-10 12:26:57 +0100

    added models

--- src/main/java/dk/kea/kinobackend/model/TheaterHall.java
+++ src/main/java/dk/kea/kinobackend/model/TheaterHall.java
@@ -1,4 +1,64 @@
 package dk.kea.kinobackend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "theater_halls")
 public class TheaterHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int hall_id;
+    private int cinema_id;
+    private String name;
+    private int seats;
+    private int rows;
+
+    public TheaterHall() {}
+
+    public TheaterHall(int cinema_id, String name, int seats, int rows) {
+        this.cinema_id = cinema_id;
+        this.name = name;
+        this.seats = seats;
+        this.rows = rows;
+    }
+
+    public int getHall_id() {
+        return hall_id;
+    }
+
+    public void setHall_id(int hall_id) {
+        this.hall_id = hall_id;
+    }
+
+    public int getCinema_id() {
+        return cinema_id;
+    }
+
+    public void setCinema_id(int cinema_id) {
+        this.cinema_id = cinema_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
 }
